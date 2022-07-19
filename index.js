e60d6e7a8dcf8e34a7e51250ee30493f03eb601a
Utkarsh Dubey <68679990+ud733@users.noreply.github.com>
2022-07-19 17:23:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports={
+    add : function(a, b){
+        return a+b;
+    },
+    subtract : function(a, b){
+        return a-b;
+    },
+    multiply : function(a, b){
+        return a*b;
+    },
+    divide : function(a, b){
+        return a/b;
+    },
+
+};
